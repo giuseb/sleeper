6585fd311255a07e17abd9794f6ef3a11af4d5a7
Giuseppe <giuseppe.bertini@gmail.com>
2018-05-09 14:03:43 +0200

    HypnoAnal with blocking computations
    first draft, only for epochs, minutes, seconds

--- src/HypnoAnal.m
+++ src/HypnoAnal.m
@@ -1,53 +1,70 @@
 classdef HypnoAnal < handle
-   %HypnoAnal: simple calculations on hypnograms
+   %HYPNOANAL: simple calculations on hypnograms
    %
-   %   ha = HypnoAnal(hyp)
+   %   The HYPNOANAL class (pun intended) helps to analyze hypnograms, i.e.
+   %   arbitrarily long sequences of epochs (typically a few seconds each)
+   %   classified depending on vigilance states. The program “sleeper”
+   %   generates hypnograms as vectors of integers, where each value
+   %   corresponds to a specific state.
    %
-   %   where hyp is a numerical vector, builds the object. The following
-   %   name/value pair parameters may be added:
+   %   The HYPNOANAL constructor takes one such vector as a mandatory
+   %   argument and returns a HYPNOANAL object:
    %
-   %   ha = HypnoAnal(hyp, 'Epoch', s) specifies the epoch duration in
-   %   seconds (default is 10)
+   %   ha = HYPNOANAL(hyp)
    %
-   %   ha = HypnoAnal(hyp, 'Stages', {'REM', 'NREM', 'Wake'}) specifies the
-   %   states (the ones shown here are the defaults); every "1" in the
+   %   The following name/value pair parameters may be added to the
+   %   constructor call:
+   %
+   %   ha = HYPNOANAL(hyp, 'Epoch', s) specifies the epoch duration in
+   %   seconds (default is 10).
+   %
+   %   ha = HYPNOANAL(hyp, 'States', {'REM', 'NREM', 'Wake'}) specifies the
+   %   states (those shown here are the defaults); every "1" in the
    %   hystogram vector is interpreted as 'REM', every "2" is 'NREM', and
    %   so on.
    %
-   %   After constructions, you can execute any of the following:
+   %   You can verify these values by simply typing the object variable
+   %   name at the command line:
    %
-   %   ha.tot_epochs
-   %   ha.tot_seconds
-   %   ha.tot_minutes
-   %   ha.fractions
-   %   ha.n_episodes
-   %   ha.durations
-   %   ha.mean_sec_durations
-   %   ha.std_sec_durations
+   %   >> ha
    %
-   %   All these functions return an array with as many elements as there
-   %   are states, in the order specified as above.
+   %     HypnoAnal with properties:
    %
-   %   ha.changes  returns state transitions at the epoch in which they
-   %   occur, in number form. The first element is always NaN. Assuming the
-   %   default state list {'REM', 'NREM', 'Wake'}, here are the possible
-   %   transition values:
+   %       hypno: [450×1 double]
+   %      states: {'REM'  'NREM'  'Wake'}
+   %       epoch: 10
    %
-   %      0: no transition
-   %      1: REM  -> NREM     -1: NREM -> REM
-   %      2: NREM -> Wake     -2: Wake -> NREM
-   %      3: REM  -> Wake     -3: Wake -> REM
+   %   The HYPNOANAL object responds to the following aggregate methods,
+   %   which return an an array with as many elements as there are states
+   %   (in the order specified at object creation):
    %
-   %   Unique transitions can be encoded for an arbitrary number of states.
-   %   For example:
-   % 
-   
+   %   - ha.epochs
+   %   - ha.minutes
+   %   - ha.seconds
+   %   - ha.episodes
+   %   - ha.durations
+   %   - ha.mean_durations
+   %   - ha.std_durations
+   %   - ha.fractions
+   %
+   %   In addition, ha.transitions returns a table with the number of all
+   %   possible transitions
+   %
+
    %----------------------------------------------------------- Properties
    properties (SetAccess = private)
       hypno
-      changes
       states
-      epoch
+      epoch  % epoch duration in seconds
+      block  % number of epochs in a block
+      hyplen % number of epochs in the hypnogram
+   end
+
+   properties (Access = private)
+      changes % all the transitions computed as diff
+      nblocks % the number of blocks in the hypnogram
+      nstates % the number of states in the hypnogram
+      blocked % the hypongram reshaped based on block size
    end
    %------------------------------------------------------- Public Methods
    methods
@@ -57,30 +74,119 @@ classdef HypnoAnal < handle
          p.addRequired( 'hypnogram', @isnumvector)
          p.addParameter('Epoch', 10, @isnumscalar)
          p.addParameter('States', {'REM', 'NREM', 'Wake'}, @iscellstr)
+         p.addParameter('Block', 0, @isnumscalar)
          p.parse(hypnogram, varargin{:})
 
-         % assumes a vector
-         obj.hypno  = p.Results.hypnogram(:); % enforce vertical!
-         obj.states = p.Results.States;
-         obj.epoch  = p.Results.Epoch;
+         hy = p.Results.hypnogram(:); % enforce vertical!
+         obj.block  = p.Results.Block;
+         % trim the hypnogram if necessary, based on block size
+         if obj.block
+            % blocking has been specified, determine the number of blocks
+            obj.nblocks = floor(length(hy) / obj.block);
+         else
+            % whole hypnogram as a single block
+            obj.nblocks = 1;
+            obj.block = length(hy);
+         end
+         obj.hyplen  = obj.block * obj.nblocks;
+         obj.hypno   = hy(1:obj.hyplen);
+         obj.blocked = reshape(obj.hypno, obj.block, obj.nblocks);
+         obj.states  = p.Results.States;
+         obj.nstates = length(obj.states);
+         obj.epoch   = p.Results.Epoch;
          obj.changes = [NaN; diff(2.^(obj.hypno-1))];
-         
+
          % the hypnogram should not contain NaNs; warn if any
          nani = isnan(obj.hypno);
          if any(nani)
             warning('NaNs in hypnogram at positions: %d', find(nani))
          end
       end
-      
-      function rv = istransition(obj, st1, st2)
-         rv = obj.hypno(1:end-1)==st1 & obj.hypno(2:end)==st2;
+
+      %------ AGGREGATE METHODS, returning one element per state
+
+      function rv = epochs(obj)
+         % HA.EPOCHS, where HA is a HypnoAnal object, returns the number of
+         % epochs for each state scored in the hypnogram
+         rv = zeros(obj.nstates, obj.nblocks);
+         for i=1:obj.nstates
+            rv(i, :) = sum(obj.blocked==i);
+         end
       end
-      
-      function rv = tr_count(obj, st1, st2)
-         rv = sum(obj.istransition(st1, st2));
+
+      function rv = minutes(obj)
+         % HA.MINUTES, where HA is a HypnoAnal object, returns the total
+         % duration in minutes for each state scored in the hypnogram
+         rv = obj.seconds / 60;
       end
-      
-      function rv = tr_counts(obj)
+
+      function rv = seconds(obj)
+         % HA.SECONDS, where HA is a HypnoAnal object, returns the total
+         % duration in seconds for each state scored in the hypnogram
+         rv = obj.epochs * obj.epoch;
+      end
+
+      function rv = episodes(obj)
+         % HA.EPISODES, where HA is a HypnoAnal object, returns the number
+         % of scored episodes for each state in the hypnogram
+         t = obj.changes;
+         t(1) = 0;
+         for i=1:obj.n_states
+            rv(i) = sum(obj.hypno==i & t); %#ok<AGROW>
+         end
+      end
+
+      function rv = durations(obj)
+         % HA.DURATIONS, where HA is a HypnoAnal object, collects state
+         % episode durations. It returns a cell array, one cell for each
+         % scored state. Each cell contains an array of episode durations
+         % in seconds.
+         n = obj.n_states;
+         % set up the cell array to be returned
+         rv = cell(1, n);
+         % use the first epoch as a starting point
+         c_stg = obj.hypno(1);
+         c_len = 1;
+         % loop over each epoch
+         for i=2:length(obj.hypno)
+            if obj.changes(i)
+               rv{c_stg} = [rv{c_stg}; c_len * obj.epoch];
+               c_stg = obj.hypno(i);
+               c_len = 1;
+            else
+               c_len = c_len + 1;
+            end
+         end
+         rv{c_stg} = [rv{c_stg}; c_len * obj.epoch];
+      end
+
+      function rv = mean_durations(obj)
+         % HA.MEAN_DURATIONS, where HA is a HypnoAnal object, returns the
+         % mean episode durations (in seconds) for each state scored in the
+         % hypnogram
+         for i = 1:obj.n_states
+            rv(i) = mean(obj.durations{i}); %#ok<AGROW>
+         end
+      end
+
+      function rv = std_durations(obj)
+         % HA.STD_DURATIONS, where HA is a HypnoAnal object, returns the
+         % standard deviation of episode durations (in seconds) for each
+         % state scored in the hypnogram
+         for i = 1:obj.n_states
+            rv(i) = std(obj.durations{i}); %#ok<AGROW>
+         end
+      end
+
+
+      function rv = fractions(obj)
+         % HA.FRACTIONS, where HA is a HypnoAnal object, returns the
+         % fraction of time spent in each state scored in the hypnogram
+         tots = length(obj.hypno) * obj.epoch;
+         rv = obj.seconds / tots;
+      end
+
+      function rv = transitions(obj)
          i = 0;
          for st1 = 1:length(obj.states)-1
             for st2 = st1+1:length(obj.states)
@@ -96,66 +202,15 @@ classdef HypnoAnal < handle
          end
          rv = table(s1', s2', ct', 'variablenames', {'Before' 'After' 'Count'});
       end
-      
-      function rv = tot_seconds(obj)
-         rv = obj.tot_epochs * obj.epoch;
+
+      function rv = istransition(obj, st1, st2)
+         rv = obj.hypno(1:end-1)==st1 & obj.hypno(2:end)==st2;
       end
-      
-      function rv = tot_minutes(obj)
-         rv = obj.tot_seconds / 60;
+
+      function rv = tr_count(obj, st1, st2)
+         rv = sum(obj.istransition(st1, st2));
       end
-      
-      function rv = fractions(obj)
-         tots = length(obj.hypno) * obj.epoch;
-         rv = obj.tot_seconds / tots;         
-      end
-      
-      function rv = n_episodes(obj)
-         for i=1:obj.n_states
-            rv(i) = sum(obj.hypno==i & obj.changes); %#ok<AGROW>
-         end
-      end
-      
-      function rv = mean_sec_durations(obj)
-         d = obj.durations;
-         for i = 1:obj.n_states
-            rv(i) = mean(d{i} * obj.epoch); %#ok<AGROW>         
-         end
-      end
-      
-      function rv = std_sec_durations(obj)
-         d = obj.durations;
-         for i = 1:obj.n_states
-            rv(i) = std(d{i} * obj.epoch); %#ok<AGROW>         
-         end
-      end
-      
-      function rv = durations(obj)
-         n = obj.n_states;
-         % set up the cell array to be returned
-         rv = cell(1, n);
-         % use the first epoch as a starting point
-         c_stg = obj.hypno(1);
-         c_len = 1;
-         % loop over each epoch
-         for i=2:length(obj.hypno)
-            if obj.changes(i)
-               rv{c_stg} = [rv{c_stg}; c_len];
-               c_stg = obj.hypno(i);
-               c_len = 1;
-            else
-               c_len = c_len + 1;
-            end
-         end
-         rv{c_stg} = [rv{c_stg}; c_len];
-      end
-      
-      function rv = tot_epochs(obj)
-         for i=1:obj.n_states
-            rv(i) = sum(obj.hypno==i); %#ok<AGROW>
-         end
-      end
-      
+
       function rv = n_states(obj)
          rv = length(obj.states);
       end
--- test/test_hypnoanal.m
+++ test/test_hypnoanal.m
@@ -1,7 +1,48 @@
-clear
+% testing the HypnoAnal class using script-based unit tests
+% see Matlab documentation for more on how to write and execute tests
+
+% the default test hypnogram
 load test/test_hypnogram.mat
 ha = HypnoAnal(hypnogram);
+def_states = {'REM' 'NREM' 'Wake'};
+def_epoch  = 10;
 
-disp(ha)
+%% Test-01: make sure an object is created
+assert(isa(ha, 'HypnoAnal'))
 
-disp([hypnogram ha.changes])
+%% Test-02: the object contains default values
+assert(isequal(ha.states, def_states))
+assert(ha.epoch == def_epoch)
+
+%% Test-03: different parameters can be defined at object creation
+st =  {'uno', 'due'};
+ep = 4;
+t = HypnoAnal(hypnogram, 'states', st, 'epoch', ep);
+assert(isequal(t.states, st));
+assert(t.epoch == ep)
+
+%% Test-04: epoch, seconds, minutes
+e1 = sum(hypnogram==1);
+e2 = sum(hypnogram==2);
+e3 = sum(hypnogram==3);
+
+assert(isequal(ha.epochs, [e1, e2, e3]'));
+ep = [def_epoch*e1; def_epoch*e2; def_epoch*e3];
+assert(isequal(ha.seconds, ep))
+assert(isequal(ha.minutes, ep/60))
+
+%% Test-05: find transitions in a short, dummy hypnogram
+hy = [1 1 2 3 3 2 3 1 1 NaN 1 3 1];
+t = HypnoAnal(hy);
+assert(isequal(t.transitions.Count, [1 0 1 2 2 1]'))
+
+%% Test-06: getting epoch counts by state and block
+hy = [1 2 3 1 2 3 1 1 1 1 1 1 2 3 2 3 2 3];
+% the 18 epochs will be analyzed in groups of 3
+t = HypnoAnal(hy, 'block', 3);
+out = [
+     1     1     3     3     0     0
+     1     1     0     0     2     1
+     1     1     0     0     1     2
+];
+assert(isequal(t.epochs, out))
